3ce2ebe643c5a91cef8b406c7db30a392e2f3aa4
diffsky <matthew.hadley@gmail.com>
2014-05-25 10:43:21 -0700

    use cb to know task ended

--- gulp/clean.js
+++ gulp/clean.js
@@ -4,7 +4,7 @@ var rimraf = require('rimraf');
 
 module.exports = function(gulp){
   // clean the workspace
-  return gulp.task('clean', function (cb) {
+  gulp.task('clean', function (cb) {
     rimraf('test/out', cb);
   });
 };
